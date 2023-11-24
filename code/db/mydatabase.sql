da9bb1b891f3e862ab102a78d502fdec7d6adec7
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-11-24 11:56:08 +0100

    db

--- code/db/mydatabase.sql
+++ code/db/mydatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Nov 23, 2023 at 08:14 PM
+-- Generation Time: Nov 24, 2023 at 10:55 AM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.8
 
@@ -42,21 +42,8 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `post_id`, `user_id`, `username`, `profile_image`, `comment_text`, `date`) VALUES
-(1, 8, 3, 'lalachan', '.jpg', 'This is beautiful.', '2023-11-22 10:04:45'),
-(2, 8, 3, 'lalachan', '.jpg', 'Nice view.', '2023-11-22 10:06:46'),
-(3, 8, 3, 'lalachan', '.jpg', 'This would be great.', '2023-11-22 10:26:26'),
-(4, 44, 3, 'lalachan', '.jpg', 'comment', '2023-11-23 14:37:52'),
-(5, 44, 3, 'lalachan', '.jpg', 'This is beautiful.', '2023-11-23 14:39:03'),
-(6, 44, 3, 'lalachan', '.jpg', 'Nice view.', '2023-11-23 14:40:04'),
-(7, 44, 3, 'lalachan', '.jpg', 'This would be great.', '2023-11-23 14:43:02'),
-(8, 44, 3, 'lalachan', '.jpg', 'Nice view.', '2023-11-23 14:43:28'),
-(9, 44, 3, 'lalachan', '.jpg', 'comment', '2023-11-23 14:58:25'),
-(10, 44, 3, 'lalachan', '.jpg', 'This is beautiful.', '2023-11-23 14:59:07'),
-(11, 44, 3, 'lalachan', '.jpg', 'This would be great.', '2023-11-23 15:25:42'),
-(12, 44, 3, 'lalachan', '.jpg', 'This would be great.', '2023-11-23 15:34:48'),
-(13, 44, 3, 'lalachan', '.jpg', 'comment', '2023-11-23 15:39:19'),
-(14, 44, 3, 'lalachan', '.jpg', 'Nice view.', '2023-11-23 15:40:32'),
-(15, 44, 3, 'lalachan', '.jpg', 'Nice view.', '2023-11-23 19:53:28');
+(19, 45, 10, 'ruru', 'ruru.jpg', 'This is beautiful.', '2023-11-24 10:10:02'),
+(20, 48, 11, 'lala', 'logo.png', 'This is beautiful.', '2023-11-24 10:30:44');
 
 -- --------------------------------------------------------
 
@@ -82,13 +69,6 @@ CREATE TABLE `likes` (
   `post_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `likes`
---
-
-INSERT INTO `likes` (`id`, `user_id`, `post_id`) VALUES
-(3, 3, 8);
-
 -- --------------------------------------------------------
 
 --
@@ -112,8 +92,8 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `user_id`, `likes`, `image`, `caption`, `hashtags`, `date`, `username`, `profile_image`) VALUES
-(43, 3, 0, 'captured_image_1700746985.png', 'stamp test1', '#camp', '2023-11-23 13:43:05', 'lalachan', '.jpg'),
-(44, 3, 0, 'uploaded_image_1700747409.jpg', 'akita', '#beautiful', '2023-11-23 13:50:09', 'lalachan', '.jpg');
+(48, 10, 0, 'uploaded_image_1700821814.jpg', 'stamp test1', '#stamp', '2023-11-24 10:30:14', 'ruru', 'ruru.jpg'),
+(49, 10, 0, 'captured_image_1700823198.png', 'stamp test1', '#stamp', '2023-11-24 10:53:18', 'ruru', 'ruru.jpg');
 
 -- --------------------------------------------------------
 
@@ -124,23 +104,25 @@ INSERT INTO `posts` (`id`, `user_id`, `likes`, `image`, `caption`, `hashtags`, `
 CREATE TABLE `users` (
   `id` int NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(50) NOT NULL,
   `image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `followers` int DEFAULT '0',
   `following` int DEFAULT '0',
   `post` int DEFAULT '0',
   `bio` text,
-  `notification` tinyint(1) NOT NULL DEFAULT '1'
+  `notification` tinyint(1) NOT NULL DEFAULT '1',
+  `email_confirm_token` varchar(32) NOT NULL,
+  `email_confirmed` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `image`, `followers`, `following`, `post`, `bio`, `notification`) VALUES
-(3, 'lalachan', '27eeed98161b93ae1e3e185b2f143744', 'ruruover1105@gmail.com', '.jpg', 0, 0, 42, 'Hello :0                                          ', 1),
-(4, 'moko', '27eeed98161b93ae1e3e185b2f143744', 'moko0226@gmail.com', 'moko.jpg', 0, 0, 0, 'This is moko :0                                          ', 1);
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `image`, `followers`, `following`, `post`, `bio`, `notification`, `email_confirm_token`, `email_confirmed`) VALUES
+(10, 'ruru', 'aed64604da40b52ea0edf963840fc53d', 'ruruover1105@gmail.com', 'ruru.jpg', 0, 0, 5, 'not set yet', 1, 'a82b7ef3d096908202be3f333354e5dc', 1),
+(11, 'lala', 'aed64604da40b52ea0edf963840fc53d', 'ruruover1105@gmail.com', 'default.png', 0, 0, 0, 'not set yet', 1, '27e164730a30915c0cbd064dbea799be', 1);
 
 --
 -- Indexes for dumped tables
@@ -184,7 +166,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `followings`
@@ -196,19 +178,19 @@ ALTER TABLE `followings`
 -- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
