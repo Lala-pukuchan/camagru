4c6e5c03eb497b30a606f1eed8204e6d4e048cf4
小林瑠理 <kobayashi@kobayashis-MacBook-Pro.local>
2023-12-02 11:05:23 +0100

    public gallery

--- code/includes/header_public_gallery.php
+++ code/includes/header_public_gallery.php
@@ -21,19 +21,10 @@ session_start();
     <nav class="navbar">
         <div class="nav-wrapper">
             <img class="brand-img" src="assets/images/logo.png" />
-            <!--<form class="search-form" action="">
-                <input type="text" class="search-box" placeholder="search..." />
-            </form>-->
             <div class="nav-items">
-                <!--<a href="index.php" style="color: #000;"><i class="icon fas fa-home"></i></a>-->
-                <!--<i class="icon fas fa-plus"></i>-->
-                <!--<i class="icon fas fa-heart"></i>-->
-                <!--<a href="logout.php" style="color: #000;">
+                <a href="login.php" style="color: #000;">
                     <i class="fa-solid fa-right-from-bracket"></i>
                 </a>
-                <div class="icon user-profile">
-                    <a href="profile.php" style="color: #000;"><i class="icon fas fa-user"></i></a>
-                </div>-->
             </div>
         </div>
     </nav>
